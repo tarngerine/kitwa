ce96ec20076c2f2f717f0ba9d704f4afe2e9c3fc
Julius Tarng <julius@tarng.com>
2020-02-23 01:02:04 -0600

    Hook up to backend, get save/load working

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 let userId = '696969';
 
-// Data - Default graph data structure
-let dataGraph = {
-  id: '1',
-  nodes: [],
-  userId: userId,
-}
 
-let dataNodes = [];
+// Data - Starting data graph
+let data = {};
+
+let load = (renderCallback) => {
+  fetch('https://julius-kitwa.builtwithdark.com/load', {
+    method: 'post',
+    body: JSON.stringify({
+      userId: userId
+    })
+  }).then(response => response.json())
+  .then(d => {
+    console.log(d)
+    if (d.status !== 200) { throw(d.status) }
+    data = d;
+    renderCallback(data.graph);
+  })
+  .catch(err => {
+    console.log('error code: ' + err);
+    data = {
+      graph: {
+        graphId: uuidv4(),
+        nodes: [],
+        userId: userId,
+        updated: new Date(),
+      },
+      nodes: []
+    };
+    renderCallback(data.graph);
+  });
+}
 
 // Data - create new node data structure
 let newNode = () => {
@@ -19,17 +42,18 @@ let newNode = () => {
       y: 0,
       z: 0
     },
-    id: uuidv4(),
+    nodeId: uuidv4(),
     userId: '696969',
+    updated: new Date(),
   }
 }
 
 // Event - Add new node
 let addNode = () => {
   let n = newNode();
-  dataNodes.push(n);
-  dataGraph.nodes.push(n.id);
-  render(dataGraph);
+  data.nodes.push(n);
+  data.graph.nodes.push(n.nodeId);
+  render(data.graph);
 }
 
 // View - Load and clear templates
@@ -48,9 +72,11 @@ loadTemplates();
 let render = (graph) => {
   let app = document.querySelector('#app');
   graph.nodes.forEach(nodeId => {
-    let node = dataNodes.find(n => n.id == nodeId);
+    let node = data.nodes.find(n => n.nodeId == nodeId);
+    console.log(data.nodes)
+
     // Update existing nodes
-    let n = app.querySelector(`[data-d-id="${node.id}"]`);
+    let n = app.querySelector(`[data-d-id="${node.nodeId}"]`);
     if (n !== null) {
       let c = n.querySelector('[data-d-content]');
       if (c.value !== node.content) {
@@ -61,13 +87,12 @@ let render = (graph) => {
 
     // Add nodes that don't exist
     let t = templates[node.type].cloneNode(true);
-    t.setAttribute('data-d-id', node.id);
+    t.setAttribute('data-d-id', node.nodeId);
     t.setAttribute('data-d-type', node.type);
     t.querySelector('[data-d-content]').value = node.content;
     app.appendChild(t);
   });
 }
-render(dataGraph);
 
 // Event - Autosave
 let saveTimeout;
@@ -80,20 +105,51 @@ let save = target => {
       n = n.parentNode;
     }
     // Find the node in data, diff & update it
-    let oldIndex = dataNodes.findIndex(node =>
-      node.id == n.attributes['data-d-id'].value);
-    if (dataNodes[oldIndex].content !== target.value) {
-      dataNodes[oldIndex].content = target.value
+    let nodeIndex = data.nodes.findIndex(node =>
+      node.nodeId == n.attributes['data-d-id'].value);
+    if (data.nodes[nodeIndex].content !== target.value) {
+      data.nodes[nodeIndex].content = target.value;
+      data.nodes[nodeIndex].updated = new Date();
     }
-    showStatus('Saved!');
-    console.log(dataGraph)
+
+    let lastUpdatedNode = data.nodes.reduce((newest, node) => (newest.updated || 0) > node.updated ? newest : node, data.graph.updated);
+    data.graph.updated = lastUpdatedNode.updated;
+
+    sync((response) => {
+      if (response.status == 200) {
+        showStatus('Saved!');
+      }
+    });
   }, 1000);
 }
 
-// Backend
-let sync = () => {
+// Backend - sync data upwards
+let sync = (callback) => {
+  // Send latest update, allow server to resolve conflicts, and send back the latest
 
+  // OR send latest operation(s), and get a confirm back when an operation has been processed
+
+  // OR lock by node, maintain lock list on server, dont allow edit on client until it's confirmed that you have ownership
+  // when do you decide to unlock it? inactivity after x minutes (replicate that on clientside? last edit time!
+  // if last edit time > 5 minutes, re-request lock)
+
+  fetch('https://julius-kitwa.builtwithdark.com/save', {
+    method: 'post',
+    body: JSON.stringify(data)
+  }).then(response => response.json())
+  .then(d => {
+    console.log('response:')
+    console.log(d)
+    callback(d)
+  })
+  .catch(err => console.log(err));
 }
+
+// ---
+
+load(render);
+
+// ---
 
 // Helpers
 let statusTimeout;
